c3f90a9da8ade6201a0b729d080f7811087906e2
ashevchuk <dev.ashevchuk@gmail.com>
2020-07-15 10:55:17 +0300

    code cleanup

--- ac.pl
+++ ac.pl
@@ -13,201 +13,231 @@ use Digest::CRC      ();
 use Crypt::Mode::ECB ();
 
 use constant {
-    PORT_DEVICE         => 6444,
-    KEY_SIGN            => 'xhdiwjnchekd4d512chdjx5d8e4c394D2D7S',
-    DEVICE_BLOCK_LEN    => 16,
-    DEVICE_RESPONSE_LEN => 104
+    PORT    => 6444,
+    TIMEOUT => 5
 };
 
 use constant {
-    TEMP_VALUE_MIN      => 17,
-    TEMP_VALUE_MAX      => 30,
+    BLOCK_LEN    => 16,
+    RESPONSE_LEN => 104
 };
 
 use constant {
-    STATUS_ON  => 0x01,
-    STATUS_OFF => 0x00
+    TEMP_MIN => 17,
+    TEMP_MAX => 30
 };
 
-my $key = Digest::MD5::md5( KEY_SIGN );
-
-my $ecb = Crypt::Mode::ECB->new( AES => 0 );
-
-my @packet_tmpl = (
-    0x5a, 0x5a, 0x01, 0x11, 0x68, 0x00, 0x20, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-);
-my @command_tmpl = (
-    0xaa, 0x20, 0xac, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x41, 0x81,
-    0x00, 0xff, 0x03, 0xff, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-);
-
-my $enum = {
-    fan => {
-        input => {
-            type => "s",
-            set  => sub { $_[0]->[0x0d] = $_[1] }
-        },
-        parse => sub { List::Util::min( ( $_[0]->[0x03] & 0x7f ), 0x65 ) },
-        val => {
-            auto   => 0x65,
-            high   => 0x50,
-            medium => 0x3c,
-            low    => 0x28,
-            silent => 0x14
-        }
-    },
-    mode => {
-        input => {
-            type => "s",
-            set  => sub {
-                $_[0]->[0x0c] &= ~0xe0;
-                $_[0]->[0x0c] |= ( $_[1] << 5 ) & 0xe0;
-              }
-        },
-        parse => sub { ( $_[0]->[0x02] & 0xe0 ) >> 5 },
-        val   => {
-            auto => 0x01,
-            cool => 0x02,
-            dry  => 0x03,
-            heat => 0x04,
-            fan  => 0x05
-        }
-    },
-    swing => {
-        input => {
-            type => "s",
-            set => sub { $_[0]->[0x11] &= ~0x0f; $_[0]->[0x11] |= $_[1] & 0x0f }
-        },
-        parse => sub { $_[0]->[0x07] & 0x0f },
-        val   => {
-            off        => 0x00,
-            vertical   => 0x0c,
-            horizontal => 0x03,
-            both       => 0x0f
-        }
-    },
-    power => {
-        input => {
-            type => "s",
-            set =>
-              sub { $_[0]->[0x0b] &= ~0x01; $_[0]->[0x0b] |= $_[1] ? 0x01 : 0 }
-        },
-        parse => sub { ( $_[0]->[0x01] & 0x01 ) > 0 ? STATUS_ON : STATUS_OFF },
-        val => {
-            off => STATUS_OFF,
-            on  => STATUS_ON
-        }
-    },
-    buzzer => {
-        input => {
-            type => "s",
-            set =>
-              sub { $_[0]->[0x0b] &= ~0x42; $_[0]->[0x0b] |= $_[1] ? 0x42 : 0 }
-        },
-        parse => sub { STATUS_OFF },
-        val   => {
-            off => STATUS_OFF,
-            on  => STATUS_ON
-        }
-    },
-    error => {
-        parse => sub { ( $_[0]->[0x01] & 0x80 ) > 0 ? STATUS_ON : STATUS_OFF },
-        val => {
-            no  => STATUS_OFF,
-            yes => STATUS_ON
-        }
-    },
-    temp => {
-        input => {
-            type => "i",
-            set  => sub {
-                $_[0]->[0x0c] &= ~0x1f;
-                $_[0]->[0x0c] |= ( $_[1] & 0x0f ) | ( ( $_[1] << 4 ) & 0x10 );
-              }
-        },
-        parse => sub { ( $_[0]->[0x02] & 0x0f ) + 16 },
-        val => { map { ( $_, $_ ) } ( TEMP_VALUE_MIN .. TEMP_VALUE_MAX ) }
-    },
-    eco => {
-        input => {
-            type => "s",
-            set  => sub { $_[0]->[0x13] = $_[1] ? 0xff : 0 }
-        },
-        parse => sub { ( $_[0]->[0x09] & 0x10 ) > 0 ? STATUS_ON : STATUS_OFF },
-        val => {
-            off => STATUS_OFF,
-            on  => STATUS_ON
-        }
-    },
-    turbo => {
-        input => {
-            type => "s",
-            set  => sub { $_[0]->[0x14] = $_[1] ? 0x02 : 0 }
-        },
-        parse => sub { ( $_[0]->[0x0a] & 0x02 ) > 0 ? STATUS_ON : STATUS_OFF },
-        val => {
-            off => STATUS_OFF,
-            on  => STATUS_ON
-        }
-    },
-    temp_int => {
-        parse => sub { ( $_[0]->[0x0b] - 50 ) / 2.0 },
-    },
-    temp_ext => {
-        parse => sub { ( $_[0]->[0x0c] - 50 ) / 2.0 },
-    },
+use constant {
+    OFF => 0x00,
+    ON  => 0x01
 };
 
-my $enum_val = {
-    map {
-        my $type = $_;
-        (
-            $type => {
-                val => {
-                    map { ( $enum->{$type}->{val}->{$_}, $_ ) } keys %{
-                        exists $enum->{$type}->{val}
-                        ? $enum->{$type}->{val}
-                        : {}
+use constant {
+    OPT_STR => "s",
+    OPT_INT => "i"
+};
+
+use constant {
+    PACKET => [
+        0x5a, 0x5a, 0x01, 0x11, 0x68, 0x00, 0x20, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    ]
+};
+
+use constant {
+    COMMAND => [
+        0xaa, 0x20, 0xac, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x03, 0x41, 0x81, 0x00, 0xff, 0x03, 0xff,
+        0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    ]
+};
+
+use constant { KEY_SIGN => 'xhdiwjnchekd4d512chdjx5d8e4c394D2D7S' };
+
+use constant { KEY => Digest::MD5::md5(KEY_SIGN) };
+
+use constant {
+    SETTINGS => {
+        fan => {
+            input => {
+                type => OPT_STR,
+                set  => sub { $_[0]->[0x0d] = $_[1] }
+            },
+            parse => sub { List::Util::min( ( $_[0]->[0x03] & 0x7f ), 0x65 ) },
+            val => {
+                auto   => 0x65,
+                high   => 0x50,
+                medium => 0x3c,
+                low    => 0x28,
+                silent => 0x14
+            }
+        },
+        mode => {
+            input => {
+                type => OPT_STR,
+                set  => sub {
+                    $_[0]->[0x0c] &= ~0xe0;
+                    $_[0]->[0x0c] |= ( $_[1] << 0x05 ) & 0xe0;
+                  }
+            },
+            parse => sub { ( $_[0]->[0x02] & 0xe0 ) >> 0x05 },
+            val   => {
+                auto => 0x01,
+                cool => 0x02,
+                dry  => 0x03,
+                heat => 0x04,
+                fan  => 0x05
+            }
+        },
+        swing => {
+            input => {
+                type => OPT_STR,
+                set =>
+                  sub { $_[0]->[0x11] &= ~0x0f; $_[0]->[0x11] |= $_[1] & 0x0f }
+            },
+            parse => sub { $_[0]->[0x07] & 0x0f },
+            val   => {
+                off        => 0x00,
+                vertical   => 0x0c,
+                horizontal => 0x03,
+                both       => 0x0f
+            }
+        },
+        power => {
+            input => {
+                type => OPT_STR,
+                set  => sub {
+                    $_[0]->[0x0b] &= ~0x01;
+                    $_[0]->[0x0b] |= $_[1] ? ON : OFF;
+                  }
+            },
+            parse => sub { ( $_[0]->[0x01] & 0x01 ) > OFF ? ON : OFF },
+            val => {
+                off => OFF,
+                on  => ON
+            }
+        },
+        buzzer => {
+            input => {
+                type => OPT_STR,
+                set  => sub {
+                    $_[0]->[0x0b] &= ~0x42;
+                    $_[0]->[0x0b] |= $_[1] ? 0x42 : OFF;
+                  }
+            },
+            parse => sub { OFF },
+            val   => {
+                off => OFF,
+                on  => ON
+            }
+        },
+        error => {
+            parse => sub { ( $_[0]->[0x01] & 0x80 ) > OFF ? ON : OFF },
+            val => {
+                no  => OFF,
+                yes => ON
+            }
+        },
+        temp => {
+            input => {
+                type => OPT_INT,
+                set  => sub {
+                    $_[0]->[0x0c] &= ~0x1f;
+                    $_[0]->[0x0c] |=
+                      ( $_[1] & 0x0f ) | ( ( $_[1] << 0x04 ) & 0x10 );
+                  }
+            },
+            parse => sub { ( $_[0]->[0x02] & 0x0f ) + BLOCK_LEN },
+            val => { map { ( $_, $_ ) } ( TEMP_MIN .. TEMP_MAX ) }
+        },
+        eco => {
+            input => {
+                type => OPT_STR,
+                set  => sub { $_[0]->[0x13] = $_[1] ? 0xff : OFF }
+            },
+            parse => sub { ( $_[0]->[0x09] & 0x10 ) > OFF ? ON : OFF },
+            val => {
+                off => OFF,
+                on  => ON
+            }
+        },
+        turbo => {
+            input => {
+                type => OPT_STR,
+                set  => sub { $_[0]->[0x14] = $_[1] ? 0x02 : OFF }
+            },
+            parse => sub { ( $_[0]->[0x0a] & 0x02 ) > OFF ? ON : OFF },
+            val => {
+                off => OFF,
+                on  => ON
+            }
+        },
+        temp_int => {
+            parse => sub { ( $_[0]->[0x0b] - 0x32 ) / 0x02 },
+        },
+        temp_ext => {
+            parse => sub { ( $_[0]->[0x0c] - 0x32 ) / 0x02 },
+        },
+    }
+};
+
+use constant {
+    SETTINGS_VAL => {
+        map {
+            my $type = $_;
+            (
+                $type => {
+                    val => {
+                        exists SETTINGS->{$type}->{val}
+                        ? map { ( SETTINGS->{$type}->{val}->{$_}, $_ ) }
+                          keys %{ SETTINGS->{$type}->{val} }
+                        : ()
                     }
                 }
-            }
-          )
-    } keys %{$enum}
+              )
+        } keys %{ +SETTINGS }
+    }
 };
 
 sub inflate ($) {
-    my ($data) = @_;
-    return [ map { ord } split //, $data ];
+    [ map { ord } split //, $_[0] ];
 }
 
 sub deflate (+@) {
-    my ($data) = @_;
-    return join "", map { chr } @{$data};
+    join "", map { chr } @{ $_[0] };
+}
+
+sub encrypt ($) {
+    inflate ( Crypt::Mode::ECB->new( AES => 0 )->encrypt( deflate( $_[0] ), KEY ) );
+}
+
+sub decrypt ($) {
+    inflate ( Crypt::Mode::ECB->new( AES => 0 )->decrypt( deflate( $_[0] ) , KEY ) );
 }
 
 sub get_cmd {
-    my $data = [@command_tmpl];
+    my $data = [ @{ +COMMAND } ];
     push @{$data},
       Digest::CRC->new(
         width  => 8,
         init   => 0x00,
         xorout => 0x00,
         refout => 1,
-        poly   => 0x131,
+        poly   => 0x0131,
         refin  => 1,
         cont   => 1
-      )->add( deflate [ @{$data}[ 10 .. $#$data ] ] )->digest();
+      )->add( deflate [ @{$data}[ 0x0a .. $#$data ] ] )->digest();
     return $data;
 }
 
 sub set_cmd {
     my ($settings) = @_;
 
-    my $data = [@command_tmpl];
+    my $data = [ @{ +COMMAND } ];
 
     $data->[0x01] = 0x23;
     $data->[0x09] = 0x02;
@@ -215,8 +245,8 @@ sub set_cmd {
     push @{$data}, (0x00) x 3;
 
     foreach my $key ( keys %{$settings} ) {
-        $enum->{$key}->{input}->{set}->( $data, $settings->{$key} )
-          if exists $enum->{$key}->{input}->{set};
+        SETTINGS->{$key}->{input}->{set}->( $data, $settings->{$key} )
+          if exists SETTINGS->{$key}->{input}->{set};
     }
 
     push @{$data},
@@ -225,30 +255,30 @@ sub set_cmd {
         init   => 0x00,
         xorout => 0x00,
         refout => 1,
-        poly   => 0x131,
+        poly   => 0x0131,
         refin  => 1,
         cont   => 1
-      )->add( deflate [ @{$data}[ 10 .. $#$data ] ] )->digest();
+      )->add( deflate [ @{$data}[ 0x0a .. $#$data ] ] )->digest();
 
     return $data;
 }
 
 sub packet {
     my ( $device_id, $command ) = @_;
-    my $packet = [@packet_tmpl];
+    my $packet = [ @{ +PACKET } ];
 
-    @{$packet}[ 20, 25 ] =
+    @{$packet}[ 0x14, 0x19 ] =
       @{ inflate reverse( pack( 'H*', sprintf( "%.2x", $device_id ) ) ) };
 
     push @{$command},
-      255 - List::Util::sum0( @{$command}[ 1 .. $#$command ] ) % 256 + 1;
+      0xff - List::Util::sum0( @{$command}[ 0x01 .. $#$command ] ) % 0x0100 + 0x01;
 
-    my $pad = DEVICE_BLOCK_LEN - ( scalar( @{$command} ) % DEVICE_BLOCK_LEN );
+    my $pad = BLOCK_LEN - ( scalar( @{$command} ) % BLOCK_LEN );
     push @{$command}, ($pad) x $pad;
 
-    push @{$packet}, @{ inflate $ecb->encrypt( deflate($command), $key ) };
+    push @{$packet}, @{ encrypt( $command ) };
 
-    $packet->[0x04] = scalar( @{$packet} ) + 16;
+    $packet->[0x04] = scalar( @{$packet} ) + BLOCK_LEN;
 
     push @{$packet},
       @{ inflate Digest::MD5::md5( deflate($packet) . KEY_SIGN ) };
@@ -273,9 +303,9 @@ sub device_settings {
 
     my $body = [ @{$data}[ 0x0a .. $#$data ] ];
 
-    foreach my $key ( keys %{$enum} ) {
-        if ( exists $enum->{$key}->{parse} ) {
-            $response->{$key} = $enum->{$key}->{parse}->($body);
+    foreach my $key ( keys %{ +SETTINGS } ) {
+        if ( exists SETTINGS->{$key}->{parse} ) {
+            $response->{$key} = SETTINGS->{$key}->{parse}->($body);
         }
     }
 
@@ -289,10 +319,10 @@ sub response_val {
 
     my $body = [ @{$data}[ 0x0a .. $#$data ] ];
 
-    foreach my $key ( keys %{$enum} ) {
-        if ( exists $enum->{$key}->{parse} ) {
-            my $value = $enum->{$key}->{parse}->($body);
-            $response->{$key} = $enum_val->{$key}->{val}->{$value} // $value;
+    foreach my $key ( keys %{ +SETTINGS } ) {
+        if ( exists SETTINGS->{$key}->{parse} ) {
+            my $value = SETTINGS->{$key}->{parse}->($body);
+            $response->{$key} = SETTINGS_VAL->{$key}->{val}->{$value} // $value;
         }
     }
 
@@ -303,12 +333,12 @@ sub settings {
     my ( $new, $old ) = @_;
     my $combined = {};
 
-    foreach my $key ( keys %{$enum} ) {
-        if ( exists $enum->{$key}->{input} ) {
+    foreach my $key ( keys %{ +SETTINGS } ) {
+        if ( exists SETTINGS->{$key}->{input} ) {
             $combined->{$key} =
               (       exists( $new->{$key} )
-                  and exists( $enum->{$key}->{val}->{ $new->{$key} } ) )
-              ? $enum->{$key}->{val}->{ $new->{$key} }
+                  and exists( SETTINGS->{$key}->{val}->{ $new->{$key} } ) )
+              ? SETTINGS->{$key}->{val}->{ $new->{$key} }
               : $old->{$key};
         }
     }
@@ -323,17 +353,17 @@ sub send_request {
         Domain   => IO::Socket::AF_INET,
         Type     => IO::Socket::SOCK_STREAM,
         Proto    => 'tcp',
-        Timeout  => 5,
-        PeerPort => PORT_DEVICE,
+        Timeout  => TIMEOUT,
+        PeerPort => PORT,
         PeerHost => $device_ip
     ) or die "Socket error: $@";
 
     $client->send( deflate $data) == scalar @{$data}
       or $client->close(), die "Send error";
-    $client->recv( my $buffer, DEVICE_RESPONSE_LEN );
+    $client->recv( my $buffer, RESPONSE_LEN );
     $client->close();
 
-    length($buffer) == DEVICE_RESPONSE_LEN or die "Recv error";
+    length($buffer) == RESPONSE_LEN or die "Recv error";
 
     return inflate $buffer;
 }
@@ -341,10 +371,9 @@ sub send_request {
 sub request {
     my ( $device_ip, $data ) = @_;
     my $response =
-      inflate $ecb->decrypt(
-        deflate( [ @{ send_request( $device_ip, $data ) }[ 40 .. 87 ] ] ),
-        $key );
-    return [ @{$response}[ 0 .. $#$response - $response->[-1] ] ];
+      decrypt(
+        [ @{ send_request( $device_ip, $data ) }[ 0x28 .. 0x57 ] ] );
+    return [ @{$response}[ 0x00 .. $#$response - $response->[-1] ] ];
 }
 
 sub update {
@@ -356,11 +385,11 @@ sub update {
 
     printf(
         "Device response after update:\n%s\n",
-            join "\n",
-            (
-                map { sprintf( "%s:%s", $_, $response->{$_} ) }
-                sort keys %{$response}
-            )
+        join "\n",
+        (
+            map { sprintf( "%s:%s", $_, $response->{$_} ) }
+            sort keys %{$response}
+        )
     );
 
     return device_settings($data);
@@ -374,11 +403,11 @@ sub fetch {
     my $response = response_val($data);
     printf(
         "Device response after fetch:\n%s\n",
-            join "\n",
-            (
-                map { sprintf( "%s:%s", $_, $response->{$_} ) }
-                sort keys %{$response}
-            )
+        join "\n",
+        (
+            map { sprintf( "%s:%s", $_, $response->{$_} ) }
+            sort keys %{$response}
+        )
     );
 
     return device_settings($data);
@@ -387,15 +416,16 @@ sub fetch {
 my $option = {};
 
 Getopt::Long::GetOptions(
-    $option, qw[
+    $option,
+    qw[
       help
       set
       get
       id=i
       ip=s
-      ],
-    map { join "=", ( $_, $enum->{$_}->{input}->{type} ) }
-      grep { exists $enum->{$_}->{input} } keys %{$enum}
+    ],
+    map { join "=", ( $_, SETTINGS->{$_}->{input}->{type} ) }
+      grep { exists SETTINGS->{$_}->{input} } keys %{ +SETTINGS }
 );
 
 Pod::Usage::pod2usage(1) if exists $option->{help};
@@ -407,12 +437,13 @@ Pod::Usage::pod2usage(2)
     grep {
         my $item = $_;
         (
-                  exists( $option->{$item} )
-              and exists( $enum->{$item} )
-              and ( not scalar grep { lc $option->{$item} eq $_ }
-                keys %{ $enum->{$item}->{val} } )
+            exists( $option->{$item} ) and exists( SETTINGS->{$item} )
+              and (
+                not scalar grep { lc $option->{$item} eq $_ }
+                keys %{ SETTINGS->{$item}->{val} }
+              )
           )
-    } grep { exists $enum->{$_}->{input} } keys %{$enum}
+    } grep { exists SETTINGS->{$_}->{input} } keys %{ +SETTINGS }
   );
 
 if ( exists $option->{set} ) {
